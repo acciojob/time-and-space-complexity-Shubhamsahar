235fbc1551eedb101881fc6b1c0a97e966dbf410
Shubhamsahar <kumarshubham765484@gmail.com>
2025-06-30 22:07:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
 //your JS code here. If required.
-<h1> Time Complexityand Space Complexity</h1>
-	<p> Time complexity of binary search is efficient </p>
-	<p> <b>Space Complexity  </b>of binary search is O(n)</p>
